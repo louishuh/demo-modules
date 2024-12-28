ebbb26d7c265fd8af4b81f936e7aaec1c0a61c1e
louis <louis@woowahan.com>
2024-12-28 21:32:36 +0900

    demo-domain module을 추가

---
+++ demo-domain/src/main/java/org/example/demodomain/DemoDomainApplication.java
@@ -0,0 +1,13 @@
+package org.example.demodomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDomainApplication.class, args);
+    }
+
+}
