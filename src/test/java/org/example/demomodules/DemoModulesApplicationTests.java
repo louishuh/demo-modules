5974bec4fbaf5626569cb7153dcc9527e40af7ee
louis <louis@woowahan.com>
2024-12-28 21:20:29 +0900

    init
    intellij에서 spring boot root module을`demo-modules`라는 이름으로 생성

---
+++ src/test/java/org/example/demomodules/DemoModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demomodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoModulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
