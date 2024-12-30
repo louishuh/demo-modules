649a8a7114d41bdf1255780d5f5edcd34e06a816
louis <louis@woowahan.com>
2024-12-30 21:39:19 +0900

    demo-adapter module을 추가

---
+++ demo-adapter/src/test/java/org/example/demoadapter/DemoAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
