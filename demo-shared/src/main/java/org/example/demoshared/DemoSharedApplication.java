86ff0e3740e83eef569dc48672c5ab6c0bf61a36
louis <louis@woowahan.com>
2024-12-30 21:28:19 +0900

    demo-shared module 추가

---
+++ demo-shared/src/main/java/org/example/demoshared/DemoSharedApplication.java
@@ -0,0 +1,13 @@
+package org.example.demoshared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSharedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSharedApplication.class, args);
+    }
+
+}
---
+++ demo-shared/src/test/java/org/example/demoshared/DemoSharedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoshared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
