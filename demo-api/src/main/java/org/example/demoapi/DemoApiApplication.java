6dae1e2cb6f2c5cce426eb3770b17b778c5a6724
louis <louis@woowahan.com>
2025-01-01 15:49:27 +0900

    demo-adapter의 구현체를 scan하도록 ComponentScan Annotation 추가

--- demo-api/src/main/java/org/example/demoapi/DemoApiApplication.java
+++ demo-api/src/main/java/org/example/demoapi/DemoApiApplication.java
@@ -2,7 +2,9 @@ package org.example.demoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "org.example")
 @SpringBootApplication
 public class DemoApiApplication {
 
